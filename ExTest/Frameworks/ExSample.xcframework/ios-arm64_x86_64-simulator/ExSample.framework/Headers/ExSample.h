b5070858aba149d045ba8f8e85ffbd1c717468e9
jake.k <jake.k@hpcnt.com>
2022-06-09 12:48:36 +0900

    Implement

---
+++ ExTest/Frameworks/ExSample.xcframework/ios-arm64_x86_64-simulator/ExSample.framework/Headers/ExSample.h
@@ -0,0 +1,18 @@
+//
+//  ExSample.h
+//  ExSample
+//
+//  Created by Jake.K on 2022/06/09.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExSample.
+FOUNDATION_EXPORT double ExSampleVersionNumber;
+
+//! Project version string for ExSample.
+FOUNDATION_EXPORT const unsigned char ExSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExSample/PublicHeader.h>
+
+
--- ExTest/ViewController.swift
+++ ExTest/ViewController.swift
@@ -6,14 +6,12 @@
 //
 
 import UIKit
+import ExSample
 
 class ViewController: UIViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    a1() // a1!
   }
-
-
 }
 
